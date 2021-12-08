e6ccc98f053f62fa227ad8eaf97b96519e2eb97d
aml360 <aml360esp@gmail.com>
2021-12-08 22:19:52 +0100

    refactor: change from int array to int and use the reference

--- src/main.c
+++ src/main.c
@@ -6,8 +6,8 @@
 
 void forkexample(int num1, int num2, int num2_mitad)
 {
-  int fd1[2];
-  int fd2[2];
+  int fd1[2], fd2[2];
+
   pipe(fd1);
   pipe(fd2);
 
@@ -17,14 +17,12 @@ void forkexample(int num1, int num2, int num2_mitad)
     printf("Hello from Child1! %d\n", getpid());
     // Close read from pipe1
     close(fd1[0]);
-    unsigned long int value[1];
-    value[0] = 0;
+    unsigned long int value1 = 0;
     for (int i = 0; i <= num2_mitad; i++)
     {
-      value[0] = value[0] + i;
+      value1 += i;
     }
-
-    write(fd1[1], value, sizeof(value));
+    write(fd1[1], &value1, sizeof(value1));
     exit(0);
   }
   else
@@ -35,28 +33,27 @@ void forkexample(int num1, int num2, int num2_mitad)
       printf("Hello from Child2! %d\n", getpid());
       // Close read from pipe2
       close(fd2[0]);
-      unsigned long int value[1];
-      value[0] = 0;
+      unsigned long int value2 = 0;
       for (int i = num2_mitad; i <= num2; i++)
       {
-        value[0] = value[0] + i;
+        value2 += i;
       }
 
-      write(fd2[1], value, sizeof(value));
+      write(fd2[1], &value2, sizeof(value2));
       exit(0);
     }
     close(fd1[1]);
     close(fd2[1]);
-    unsigned long int buf_read1[1];
-    unsigned long int buf_read2[1];
+    unsigned long int buf_read[2];
 
-    read(fd1[0], buf_read1, sizeof(buf_read1));
-    read(fd2[0], buf_read2, sizeof(buf_read2));
-    long unsigned int suma_total = buf_read1[0] + buf_read2[0] + num1;
+    read(fd1[0], &buf_read[0], sizeof(unsigned long int));
+    read(fd2[0], &buf_read[1], sizeof(unsigned long int));
+
+    unsigned long int suma_total = buf_read[0] + buf_read[1] + num1;
 
     pid_t cpid = wait(NULL);
     pid_t cpid2 = wait(NULL);
-    printf("Hello from Parent!, pid: %d, value1: %ld, value2: %ld, sumatotal: %ld\n", getpid(), buf_read1[0], buf_read2[0], suma_total);
+    printf("Hello from Parent!, pid: %d, value1: %ld, value2: %ld, sumatotal: %ld\n", getpid(), buf_read[0], buf_read[1], suma_total);
   }
 }
 
